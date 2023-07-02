54fc4e74fc4f6a15a53edf3f407ed1f562a225fa
Lepkovich <6332133@gmail.com>
2023-07-02 12:20:14 +0300

    login validation done

--- frontend/scripts/form.js
+++ frontend/scripts/form.js
@@ -1,4 +1,7 @@
 (function (){
+    const processElement = null;
+    const passwordMatch = false;
+
     const Form = {
         fields: [
             {
@@ -6,6 +9,7 @@
                 id: 'full-name',
                 element:null,
                 regex: /^[А-Я][а-я]+\s*$/, //регулярка для имени (первая - заглавная, русские буквы
+                valid: false,
 
             },
             {
@@ -13,6 +17,7 @@
                 id: 'email',
                 element:null,
                 regex: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/, //регулярка для email
+                valid: false,
             },
             {
                 name: 'password',
@@ -23,11 +28,13 @@
                 (?=.*[a-z])       // should contain at least one lower case
                 (?=.*[A-Z])       // should contain at least one upper case
                 [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters  */
+                valid: false,
             },
             {
                 name: 'repeat-password',
                 id: 'repeat-password',
                 element:null,
+                valid: false,
             },
         ],
         init(){
@@ -37,15 +44,48 @@
                 item.element.onchange = function () {
                     that.validateField.call(that, item, this)
                 }
-            })
+            });
+            this.processElement = document.getElementById('process');
         },
         validateField(field, element) {
-            if(!element.value || !element.value.match(field.regex)) {
-                element.classList.add('is-invalid');
-                element.nextElementSibling.style.display = "flex";
+
+                if (!element.value || !element.value.match(field.regex)) {
+                    field.valid = false;
+                    element.classList.add('is-invalid');
+                    if (element.validationMessage) {
+                        element.nextElementSibling.innerText = element.validationMessage;
+                    }
+                    element.nextElementSibling.style.display = "flex";
+                } else {
+                    field.valid = true;
+                    element.classList.remove('is-invalid');
+                    element.nextElementSibling.style.display = "none";
+                }
+
+                if(field.name === 'repeat-password') {
+
+                    const passwordField = this.fields.find(item => item.name === 'password');
+                    console.log(passwordField.element.value);
+                    if (!element.value || element.value !== passwordField.element.value) {
+                        field.valid = false;
+                        element.classList.add('is-invalid');
+                        element.nextElementSibling.style.display = 'flex';
+                    } else {
+                        field.valid = true;
+                        element.classList.remove('is-invalid');
+                        element.nextElementSibling.style.display = "none";
+                    }
+                }
+
+
+            this.validateForm();
+        },
+        validateForm(){
+            const validForm = this.fields.every(item => item.valid);
+            if (validForm) {
+                this.processElement.classList.remove('disabled');
             } else {
-                element.classList.remove('is-invalid');
-                element.nextElementSibling.style.display = "none";
+                this.processElement.classList.add('disabled');
             }
         }
     };
