5bae0a629ba70ba1455c2a57da9bef7ccce2e4cd
Lepkovich <6332133@gmail.com>
2023-07-04 18:13:59 +0300

    routing is done

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -20,6 +20,78 @@ export class Router {
                      new Form();
                 }
             },
+            {
+                route: '#/p&l',
+                title: 'Доходы и расходы',
+                template: 'templates/p&l.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/edit-p&l',
+                title: 'Редактировать доход/расход',
+                template: 'templates/edit-p&l.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/edit-expenses',
+                title: 'Редактировать категорию расхода',
+                template: 'templates/edit-expenses.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/edit-earnings',
+                title: 'Редактировать категорию дохода',
+                template: 'templates/edit-earnings.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/earnings',
+                title: 'Доходы',
+                template: 'templates/earnings.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/expenses',
+                title: 'Расходы',
+                template: 'templates/expenses.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/add-p&l',
+                title: 'Создание дохода/расхода',
+                template: 'templates/add-p&l.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/add-expenses',
+                title: 'Создание категории расходов',
+                template: 'templates/add-expenses.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/add-earnings',
+                title: 'Создание категории доходов',
+                template: 'templates/add-earnings.html',
+                styles: 'styles/main.css',
+                load: () => {
+                }
+            },
         ]
     }
 
