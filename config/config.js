740208dcb3a032ae59bf8a031240287894d25ceb
Lepkovich <6332133@gmail.com>
2023-08-23 16:48:01 +0300

    change host to https

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 export default {
     // host: 'http://localhost:3000/api'
-    host: 'http://45.89.27.219:3000/api'
+    // host: 'http://45.89.27.219:3000/api'
+    host: 'https://cors-anywhere.herokuapp.com/http://45.89.27.219:3000/api'
 }
